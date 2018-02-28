94a0510357f05c35e75085cb959ff9bc4990ca31
Your Name <fcaminad@laas.fr>
2018-02-28 08:23:14 +0000

    ajout structures

--- RASP_PI3/spi_test_general.c
+++ RASP_PI3/spi_test_general.c
@@ -29,17 +29,26 @@
 
 //fonctions
 int16_t  fconcat (int8_t , int8_t );
-int  f_LSB (int octet);
-int  f_MSB (int octet);
+int16_t   f_LSB (int8_t octet);
+int16_t   f_MSB (int8_t octet);
 int SPIbcm2835_setup ();
 int i;
+
+struct TrameRead  rbuffer;
+struct TrameRead* ptr_rbuffer;
+
+struct TrameWrite  wbuffer;
+struct TrameWrite* ptr_wbuffer;
 
 
 int main(int argc, char **argv)
 {
-
-SPIbcm2835_setup (); // configuration SPI
-float ph = 1.57;
+ 
+ ptr_rbuffer=&rbuffer;
+ ptr_wbuffer=&wbuffer;
+  
+ SPIbcm2835_setup (); // configuration SPI
+ float ph = 1.57;
 
 
 while (1)
@@ -50,101 +59,105 @@ while (1)
  //To send 
 
  //wOdR= labs(255*cos(ph));
- wAxR= labs(255*cos(ph));
- wAxL= labs(255*cos(ph));
- wXmR= labs(4095*cos(ph));
- wXmL= labs(4095*cos(ph));
- 
- //wOdR= 0;
- //wOdL= 0;
- //wAxR= 0;
- //wAxL= 0;
- //wXmR=0;
- //wXmL= 0;
- 
- //wOdR= 0000;
- 
- //wOdL= 8000;
+ wbuffer.wOdR= 0;
+ wbuffer.wOdL= 0;
+ wbuffer.wAxR= labs(255*cos(ph));
+ wbuffer.wAxL= labs(255*cos(ph));
+ wbuffer.wXmR= labs(4095*cos(ph));
+ wbuffer.wXmL= labs(4095*cos(ph));
  
  
-  
-  
+ //wbuffer.wAxR= 50;
+ //wbuffer.wAxL= 20;
+ 
+ 
+ //wbuffer.wOdR= 1100;
+ //wbuffer.wOdL= 8000;
+ 
+ //wbuffer.wXmR=11;
+ //wbuffer.wXmL= 30;
+ 
+ 
+
   //Trame à envoyer 
-  uint8_t TxBuffer [12]={wOdRM,wOdRL,wOdLM,wOdLL,wAxRM,wAxRL,wAxLM,wAxLL,wXmRM,wXmRL,wXmLM,wXmLL};
+ // uint8_t TxBuffer [12]={wOdRM,wOdRL,wOdLM,wOdLL,wAxRM,wAxRL,wAxLM,wAxLL,wXmRM,wXmRL,wXmLM,wXmLL};
   
   //Trame à recevoir 
-  uint8_t RxBuffer [22];
+  //uint8_t RxBuffer [22];
   
   //Decoupage en octets avant envoie
-  wOdRM = f_MSB (wOdR);
-  wOdRL = f_LSB (wOdR);
-  wOdLM = f_MSB (wOdL);
-  wOdLL = f_LSB (wOdL);
-  wAxRM = f_MSB (wAxR);
-  wAxRL = f_LSB (wAxR);
-  wAxLM = f_MSB (wAxL);
-  wAxLL = f_LSB (wAxL); 
-  wXmRM = f_MSB (wXmR);
-  wXmRL = f_LSB (wXmR);
-  wXmLM = f_MSB (wXmL);
-  wXmLL = f_LSB (wXmL);                                             
+  //wOdRM = f_MSB (wOdR);
+  //wOdRL = f_LSB (wOdR);
+  //wOdLM = f_MSB (wOdL);
+  //wOdLL = f_LSB (wOdL);
+  //wAxRM = f_MSB (wAxR);
+  //wAxRL = f_LSB (wAxR);
+  //wAxLM = f_MSB (wAxL);
+  //wAxLL = f_LSB (wAxL); 
+  //wXmRM = f_MSB (wXmR);
+  //wXmRL = f_LSB (wXmR);
+  //wXmLM = f_MSB (wXmL);
+  //wXmLL = f_LSB (wXmL);                                             
                                     
                                                        
    //Activation SPI
   bcm2835_spi_begin();    
   
   //Envoie et reception des données
-  bcm2835_spi_transfernb(TxBuffer ,RxBuffer,22);                                                
+  //bcm2835_spi_transfernb(TxBuffer ,RxBuffer,22); 
+  
+  bcm2835_spi_transfernb(((uint8_t*)ptr_wbuffer) ,((uint8_t*)ptr_rbuffer),SIZE_BUFFER);   //TX,RX,size                                             
+  
   
    //Arret SPI
   bcm2835_spi_end();
   
     //RECEPTION
   //Concatenation variables après reception
-  rOdR = fconcat(RxBuffer[0], RxBuffer[1]);
-  rOdL = fconcat(RxBuffer[2], RxBuffer[3]);
+  //rOdR = fconcat(RxBuffer[0], RxBuffer[1]);
+  //rOdL = fconcat(RxBuffer[2], RxBuffer[3]);
   
-  rXrate = fconcat(RxBuffer[4], RxBuffer[5]);
-  rYrate = fconcat(RxBuffer[6], RxBuffer[7]);
-  rZrate = fconcat(RxBuffer[8], RxBuffer[9]);
-  rXacc = fconcat(RxBuffer[10], RxBuffer[11]);
-  rYacc = fconcat(RxBuffer[12], RxBuffer[13]);
-  rZacc = fconcat(RxBuffer[14], RxBuffer[15]);
-  rXmag = fconcat(RxBuffer[16], RxBuffer[17]);
-  rYmag = fconcat(RxBuffer[18], RxBuffer[19]);
-  rZmag = fconcat(RxBuffer[20], RxBuffer[21]);
+  //rXrate = fconcat(RxBuffer[4], RxBuffer[5]);
+  //rYrate = fconcat(RxBuffer[6], RxBuffer[7]);
+  //rZrate = fconcat(RxBuffer[8], RxBuffer[9]);
+  //rXacc = fconcat(RxBuffer[10], RxBuffer[11]);
+  //rYacc = fconcat(RxBuffer[12], RxBuffer[13]);
+  //rZacc = fconcat(RxBuffer[14], RxBuffer[15]);
+  //rXmag = fconcat(RxBuffer[16], RxBuffer[17]);
+  //rYmag = fconcat(RxBuffer[18], RxBuffer[19]);
+  //rZmag = fconcat(RxBuffer[20], RxBuffer[21]);
   
   //Mise à l'echelle data IMU
-  rXrate_scaled= (float) rXrate/ FACTOR_RATE ;
-  rYrate_scaled= (float) rYrate / FACTOR_RATE ;
-  rZrate_scaled= (float) rZrate / FACTOR_RATE ;
-  rXacc_scaled= (float) rXacc / FACTOR_ACC ;
-  rYacc_scaled= (float) rYacc / FACTOR_ACC ;
-  rZacc_scaled= (float) rZacc / FACTOR_ACC;
-  rXmag_scaled= (float) rXmag / FACTOR_MAG;
-  rYmag_scaled= (float) rYmag / FACTOR_MAG;
-  rZmag_scaled= (float) rZmag / FACTOR_MAG;
+  //rXrate_scaled= (float) rXrate/ FACTOR_RATE ;
+  //rYrate_scaled= (float) rYrate / FACTOR_RATE ;
+  //rZrate_scaled= (float) rZrate / FACTOR_RATE ;
+  //rXacc_scaled= (float) rXacc / FACTOR_ACC ;
+  //rYacc_scaled= (float) rYacc / FACTOR_ACC ;
+  //rZacc_scaled= (float) rZacc / FACTOR_ACC;
+  //rXmag_scaled= (float) rXmag / FACTOR_MAG;
+  //rYmag_scaled= (float) rYmag / FACTOR_MAG;
+  //rZmag_scaled= (float) rZmag / FACTOR_MAG;
   
   
   
   printf("----Sent----\n");
-  printf(" OD0 %d | OD1 %d | AX1 %d | AX2 %d | XM1 %d | XM2 %d \n",wOdR,wOdL,wAxR,wAxL,wXmR,wXmL);
+  printf(" OD0 %d | OD1 %d | AX1 %d | AX2 %d | XM1 %d | XM2 %d \n",wbuffer.wOdR,wbuffer.wOdL,wbuffer.wAxR,wbuffer.wAxL,wbuffer.wXmR,wbuffer.wXmL);
   
-  printf("----ODrive----\n");
-  printf("Received rOdR %d \n",rOdR);
-  printf("Received rOdL  %d \n",rOdL);
+  //printf("----ODrive----\n");
+  //printf("Received rOdR %d \n",rxbuffer.rOdR);
+  //printf("Received rOdL  %d \n",rxbuffer.rOdL);
   
-  printf("----IMU Scaled----\n");
-  printf("Received X rate  %f \n",rXrate_scaled);
-  printf("Received Y rate  %f \n",rYrate_scaled);
-  printf("Received Z rate  %f \n",rZrate_scaled);
-  printf("Received X accel %f  \n",rXacc_scaled);
-  printf("Received Y accel  %f  \n",rYacc_scaled);
-  printf("Received Z accel  %f  \n",rZacc_scaled);
-  printf("Received X mag  %f  \n",rXmag_scaled);
-  printf("Received Y mag  %f  \n",rYmag_scaled);
-  printf("Received Z mag  %f \n",rZmag_scaled);                                                     
-  delay(10);                                                   
+  //printf("----IMU Scaled----\n");
+  //printf("Received X rate  %f \n",rXrate_scaled);
+  //printf("Received Y rate  %f \n",rYrate_scaled);
+  //printf("Received Z rate  %f \n",rZrate_scaled);
+  //printf("Received X accel %f  \n",rXacc_scaled);
+  //printf("Received Y accel  %f  \n",rYacc_scaled);
+  //printf("Received Z accel  %f  \n",rZacc_scaled);
+  //printf("Received X mag  %f  \n",rXmag_scaled);
+  //printf("Received Y mag  %f  \n",rYmag_scaled);
+  //printf("Received Z mag  %f \n",rZmag_scaled);                                                     
+  delay(50);                                                   
                                                        
     
 
@@ -184,14 +197,13 @@ int16_t fconcat (int8_t octet_msb, int8_t octet_lsb){
   return val_concat;
  }
  
-int  f_MSB (int octet){
-  int octetmsb= (octet>>8); //on supprime les bits de poids faibles
-  return octetmsb;
+int16_t f_MSB (int8_t word){
+  return (word>>8); //on supprime les bits de poids faibles
  }
  
- int  f_LSB (int octet){
-  int octetlsb = (octet & 0x00FF); //avec un masque on supprime les bits de poids forts
-  return octetlsb;
+int16_t   f_LSB (int8_t  word){
+    
+  return (word & 0x00FF);//avec un masque on supprime les bits de poids forts
  }
  
 
