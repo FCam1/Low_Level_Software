ff75ac7098bda51ef7e2e81e2f26556ebd50869d
Your Name <fcaminad@laas.fr>
2018-03-01 12:30:55 +0000

    passage en struct pour IMU

--- RASP_PI3/spi_test_general.c
+++ RASP_PI3/spi_test_general.c
@@ -1,8 +1,3 @@
-// ---------------------------------------------------------------------------
-/*
- * Pour rasberry pi, à lancer avec SPI_test.ino
- * reçoit et envoie 4 octets avec concaténation et déconcaténation 16bits
-*/
 //----------------------------------------------------------------------------
 // Example program for bcm2835 library
 // Shows how to interface with SPI to transfer a byte to and from an SPI device
@@ -30,11 +25,8 @@
 #include <inttypes.h>
 
 //fonctions
-int16_t  fconcat (int8_t , int8_t );
-int16_t   f_LSB (int8_t octet);
-int16_t   f_MSB (int8_t octet);
 int SPIbcm2835_setup ();
-int i;
+
 
 struct TrameRead  rbuffer;
 struct TrameRead* ptr_rbuffer;
@@ -55,10 +47,10 @@ int main(int argc, char **argv)
 
 while (1)
 {
+
  ph = ph+0.01;
 
  //To send 
-
  //wOdR= labs(255*cos(ph));
  wbuffer.wOdR= 0;
  wbuffer.wOdL= 0;
@@ -70,76 +62,31 @@ while (1)
  
  //wbuffer.wAxR= 50;
  //wbuffer.wAxL= 20;
- 
- 
  //wbuffer.wOdR= 1100;
  //wbuffer.wOdL= 8000;
- 
  //wbuffer.wXmR=11;
  //wbuffer.wXmL=30;
- 
- 
-
-  //Trame à envoyer 
- // uint8_t TxBuffer [12]={wOdRM,wOdRL,wOdLM,wOdLL,wAxRM,wAxRL,wAxLM,wAxLL,wXmRM,wXmRL,wXmLM,wXmLL};
-  
-  //Trame à recevoir 
-  //uint8_t RxBuffer [22];
-  
-  //Decoupage en octets avant envoie
-  //wOdRM = f_MSB (wOdR);
-  //wOdRL = f_LSB (wOdR);
-  //wOdLM = f_MSB (wOdL);
-  //wOdLL = f_LSB (wOdL);
-  //wAxRM = f_MSB (wAxR);
-  //wAxRL = f_LSB (wAxR);
-  //wAxLM = f_MSB (wAxL);
-  //wAxLL = f_LSB (wAxL); 
-  //wXmRM = f_MSB (wXmR);
-  //wXmRL = f_LSB (wXmR);
-  //wXmLM = f_MSB (wXmL);
-  //wXmLL = f_LSB (wXmL);                                             
-                                    
+                                                                                                            
                                                        
-   //Activation SPI
+  //Activation SPI
   bcm2835_spi_begin();    
   
   //Envoie et reception des données
-  //bcm2835_spi_transfernb(TxBuffer ,RxBuffer,22); 
-  
   bcm2835_spi_transfernb(((uint8_t*)ptr_wbuffer) ,((uint8_t*)ptr_rbuffer),SIZE_BUFFER);   //TX,RX,size                                             
   
-  
-   //Arret SPI
+  //Arret SPI
   bcm2835_spi_end();
   
-    //RECEPTION
-  //Concatenation variables après reception
-  //rOdR = fconcat(RxBuffer[0], RxBuffer[1]);
-  //rOdL = fconcat(RxBuffer[2], RxBuffer[3]);
-  
-  //rXrate = fconcat(RxBuffer[4], RxBuffer[5]);
-  //rYrate = fconcat(RxBuffer[6], RxBuffer[7]);
-  //rZrate = fconcat(RxBuffer[8], RxBuffer[9]);
-  //rXacc = fconcat(RxBuffer[10], RxBuffer[11]);
-  //rYacc = fconcat(RxBuffer[12], RxBuffer[13]);
-  //rZacc = fconcat(RxBuffer[14], RxBuffer[15]);
-  //rXmag = fconcat(RxBuffer[16], RxBuffer[17]);
-  //rYmag = fconcat(RxBuffer[18], RxBuffer[19]);
-  //rZmag = fconcat(RxBuffer[20], RxBuffer[21]);
-  
   //Mise à l'echelle data IMU
-  //rXrate_scaled= (float) rXrate/ FACTOR_RATE ;
-  //rYrate_scaled= (float) rYrate / FACTOR_RATE ;
-  //rZrate_scaled= (float) rZrate / FACTOR_RATE ;
-  //rXacc_scaled= (float) rXacc / FACTOR_ACC ;
-  //rYacc_scaled= (float) rYacc / FACTOR_ACC ;
-  //rZacc_scaled= (float) rZacc / FACTOR_ACC;
-  //rXmag_scaled= (float) rXmag / FACTOR_MAG;
-  //rYmag_scaled= (float) rYmag / FACTOR_MAG;
-  //rZmag_scaled= (float) rZmag / FACTOR_MAG;
-  
-  
+  rXrate_scaled= (float) rbuffer.rXrate / FACTOR_RATE ;
+  rYrate_scaled= (float) rbuffer.rYrate / FACTOR_RATE ;
+  rZrate_scaled= (float) rbuffer.rZrate / FACTOR_RATE ;
+  rXacc_scaled= (float)  rbuffer.rXacc / FACTOR_ACC ;
+  rYacc_scaled= (float)  rbuffer.rYacc / FACTOR_ACC ;
+  rZacc_scaled= (float)  rbuffer.rZacc / FACTOR_ACC;
+  rXmag_scaled= (float)  rbuffer.rXmag / FACTOR_MAG;
+  rYmag_scaled= (float)  rbuffer.rYmag / FACTOR_MAG;
+  rZmag_scaled= (float)  rbuffer.rZmag / FACTOR_MAG;
   
   printf("----Sent----\n");
   printf(" OD0 %d | OD1 %d | AX1 %d | AX2 %d | XM1 %d | XM2 %d \n",wbuffer.wOdR,wbuffer.wOdL,wbuffer.wAxR,wbuffer.wAxL,wbuffer.wXmR,wbuffer.wXmL);
@@ -148,21 +95,19 @@ while (1)
   //printf("Received rOdR %d \n",rxbuffer.rOdR);
   //printf("Received rOdL  %d \n",rxbuffer.rOdL);
   
-  //printf("----IMU Scaled----\n");
-  //printf("Received X rate  %f \n",rXrate_scaled);
-  //printf("Received Y rate  %f \n",rYrate_scaled);
-  //printf("Received Z rate  %f \n",rZrate_scaled);
-  //printf("Received X accel %f  \n",rXacc_scaled);
-  //printf("Received Y accel  %f  \n",rYacc_scaled);
-  //printf("Received Z accel  %f  \n",rZacc_scaled);
-  //printf("Received X mag  %f  \n",rXmag_scaled);
-  //printf("Received Y mag  %f  \n",rYmag_scaled);
-  //printf("Received Z mag  %f \n",rZmag_scaled);                                                     
+  printf("----IMU Scaled----\n");
+  printf("Received X rate  %f \n",rXrate_scaled);
+  printf("Received Y rate  %f \n",rYrate_scaled);
+  printf("Received Z rate  %f \n",rZrate_scaled);
+  printf("Received X accel %f  \n",rXacc_scaled);
+  printf("Received Y accel  %f  \n",rYacc_scaled);
+  printf("Received Z accel  %f  \n",rZacc_scaled);
+  printf("Received X mag  %f  \n",rXmag_scaled);
+  printf("Received Y mag  %f  \n",rYmag_scaled);
+  printf("Received Z mag  %f \n",rZmag_scaled);
+                                                       
   delay(10);                                                   
                                                        
-    
-
-
 }
 
 return 0;
@@ -193,20 +138,7 @@ int SPIbcm2835_setup (){
 
 
 
-int16_t fconcat (int8_t octet_msb, int8_t octet_lsb){
-  int16_t val_concat = (octet_msb<<8 | octet_lsb); // concatenation des 2 octets de 8 bits
-  return val_concat;
- }
- 
-int16_t f_MSB (int8_t word){
-  return (word>>8); //on supprime les bits de poids faibles
- }
- 
-int16_t   f_LSB (int8_t  word){
-    
-  return (word & 0x00FF);//avec un masque on supprime les bits de poids forts
- }
- 
+
 
  
 
--- STM32/test_general/variables.h
+++ STM32/test_general/variables.h
@@ -10,104 +10,43 @@
 #define Y_RATE_adress           	      0x0600
 #define Z_RATE_adress           	      0x0800 
 #define X_ACC_adress         	        	0x0A00
-#define Y_ACC_adress          		        0x0C00 
-#define Z_ACC_adress          		        0x0E00  
+#define Y_ACC_adress          		       0x0C00 
+#define Z_ACC_adress          		       0x0E00  
 #define X_MAG_adress            	      0x1000
 #define Y_MAG_adress                  	0x1200 
-#define Z_MAG_adress                   	0x1400 
-#define PRODUCT_ID_adress      		0x5600 
-#define STDR_REGISTER_adress		  0x3400
-#define DIAGNOSTIC_STATUS_adress	0x3C00 
+#define Z_MAG_adress                   0x1400 
+#define PRODUCT_ID_adress      		      0x5600 
+#define STDR_REGISTER_adress		         0x3400
+#define DIAGNOSTIC_STATUS_adress	      0x3C00 
 
-#define FACTOR_RATE               200
-#define FACTOR_ACC                4000
-#define FACTOR_MAG                16384
+#define FACTOR_RATE                    200
+#define FACTOR_ACC                     4000
+#define FACTOR_MAG                     16384
 
 //Variables
-#define ZERO            		0x0000 //Following all commands in order to allow response (NSS)
+#define ZERO            		             0x0000 //Following all commands in order to allow response (NSS)
 
 #define CS_PIN              PA4
 //------------------Definition Baudrate --------------------
-#define BAUD_AX                 1000000 
-#define BAUD_XM                 1000000
-#define BAUD_ODRIVE          115200
+#define BAUD_AX                        1000000 
+#define BAUD_XM                        1000000
+#define BAUD_ODRIVE                    115200
 //------------------SPI --------------------
-#define SIZE_BUFFER          12
+#define SIZE_BUFFER                    30
 //------------------Definition des Pins --------------------
-#define PIN_DATA_CTRL_AX       PG2 
-#define PIN_DATA_CTRL_XM      PG3
+#define PIN_DATA_CTRL_AX               PG2 
+#define PIN_DATA_CTRL_XM               PG3
 //------------------Definition des Moteurs--------------------
 
-#define OD_RIGHT    0
-#define OD_LEFT     1
-#define AX_RIGHT    1
-#define AX_LEFT     2     
-#define XM_RIGHT    1
-#define XM_LEFT     2 
-#define ALL       254 // Broadcast ID
+#define OD_RIGHT                       0
+#define OD_LEFT                        1
+#define AX_RIGHT                       1
+#define AX_LEFT                        2     
+#define XM_RIGHT                       1
+#define XM_LEFT                        2 
+#define ALL                            254 // Broadcast ID
 
 //------------------Definition des Variables--------------------
-// Variables "write" to send over SPI
-//uint8_t wOdRL ; //write Odrive Right LSB
-//uint8_t wOdRM ;//MSB
-//uint8_t wOdLL ;//LSB
-//uint8_t wOdLM ;//MSB
-//uint8_t wAxRL ;//LSB
-//uint8_t wAxRM ;//MSB
-//uint8_t wAxLL ;//LSB
-//uint8_t wAxLM ;//MSB
-//uint8_t wXmRL ;//LSB
-//uint8_t wXmRM ;//MSB
-//uint8_t wXmLL ;//LSB
-//uint8_t wXmLM ;//MSB
-
-// Variables "read" received over SPI
-//uint8_t rOdRL ;//read Odrive Right LSB
-//uint8_t rOdRM ;//MSB
-//uint8_t rOdLL ;//LSB
-//uint8_t rOdLM ;//MSB
-//uint8_t rAxRL ;//LSB
-//uint8_t rAxRM ;//MSB
-//uint8_t rAxLL ;//LSB
-//uint8_t rAxLM ;//MSB
-//uint8_t rXmRL ;//LSB
-//uint8_t rXmRM ;//MSB
-//uint8_t rXmLL ;//LSB
-//uint8_t rXmLM ;//MSB
-////IMU
-uint8_t rXrateM;//LSB
-uint8_t rXrateL;//MSB
-uint8_t rYrateM;
-uint8_t rYrateL;//LSB
-uint8_t rZrateM;
-uint8_t rZrateL;//LSB
-uint8_t rXaccM;
-uint8_t rXaccL;//LSB
-uint8_t rYaccM;
-uint8_t rYaccL;//LSB
-uint8_t rZaccM;
-uint8_t rZaccL;//LSB
-uint8_t rXmagM;
-uint8_t rXmagL;//LSB
-uint8_t rYmagM;
-uint8_t rYmagL;//LSB
-uint8_t rZmagM;
-uint8_t rZmagL;//LSB
-
-////variable finales 
-//uint16_t wOdR ; 
-//uint16_t wOdL ;
-//uint16_t wAxR ;
-//uint16_t wAxL ;
-//uint16_t wXmR ;
-//uint16_t wXmL ;
-
-uint16_t rOdR ; 
-uint16_t rOdL ;
-uint16_t rAxR ;
-uint16_t rAxL ;
-uint16_t rXmR ;
-uint16_t rXmL ;
 
 //IMU
 float   rXrate_scaled;
@@ -124,74 +63,51 @@ uint8_t DIAGNOSTIC_STATUS;
 
 //------------------structures--------------------
 
-struct TrameWrite {
-//uint8_t wOdRL ; //write Odrive Right LSB
-//uint8_t wOdRM ;//MSB
-//uint8_t wOdLL ;//LSB
-//uint8_t wOdLM ;//MSB
-//uint8_t wAxRL ;//LSB
-//uint8_t wAxRM ;//MSB
-//uint8_t wAxLL ;//LSB
-//uint8_t wAxLM ;//MSB
-//uint8_t wXmRL ;//LSB
-//uint8_t wXmRM ;//MSB
-//uint8_t wXmLL ;//LSB
-//uint8_t wXmLM ;//MSB
-
-//variable finales 
+struct TrameWrite { // Variables "write" to send over SPI from the master
+//Odrive
 uint16_t wOdR ; 
 uint16_t wOdL ;
+//AX
 uint16_t wAxR ;
 uint16_t wAxL ;
+//XM
 uint16_t wXmR ;
 uint16_t wXmL ;
+
+//autre
+uint16_t a ;
+uint16_t b ;
+uint16_t c ;
+uint16_t d ;
+uint16_t e ;
+uint16_t f ;
+uint16_t g ;
+uint16_t h ;
+uint16_t i ;
+
+
 };
 
 
-struct TrameRead {
-   //Odrive
-  uint8_t rOdRL ;//read Odrive Right LSB
-  uint8_t rOdRM ;//MSB
-  uint8_t rOdLL ;//LSB
-  uint8_t rOdLM ;//MSB*
-   //AX
-  uint8_t rAxRL ;//LSB
-  uint8_t rAxRM ;//MSB
-  uint8_t rAxLL ;//LSB
-  uint8_t rAxLM ;//MSB
-    //XM
-  uint8_t rXmRL ;//LSB
-  uint8_t rXmRM ;//MSB
-  uint8_t rXmLL ;//LSB
-  uint8_t rXmLM ;//MSB
-  //IMU
-  uint8_t rXrateM;//LSB
-  uint8_t rXrateL;//MSB
-  uint8_t rYrateM;
-  uint8_t rYrateL;//LSB
-  uint8_t rZrateM;
-  uint8_t rZrateL;//LSB
-  uint8_t rXaccM;
-  uint8_t rXaccL;//LSB
-  uint8_t rYaccM;
-  uint8_t rYaccL;//LSB
-  uint8_t rZaccM;
-  uint8_t rZaccL;//LSB
-  uint8_t rXmagM;
-  uint8_t rXmagL;//LSB
-  uint8_t rYmagM;
-  uint8_t rYmagL;//LSB
-  uint8_t rZmagM;
-  uint8_t rZmagL;//LSB
-
-  uint8_t rXrate;
-  uint8_t rYrate;
-  uint8_t rZrate;
-  uint8_t rXacc;
-  uint8_t rYacc;
-  uint8_t rZacc;
-  uint8_t rXmag;
-  uint8_t rYmag;
-  uint8_t rZmag;
+struct TrameRead { // Variables "read" to send over SPI from the slave
+//Odrive
+uint16_t rOdR ; //read Odrive Right
+uint16_t rOdL ;
+//AX
+uint16_t rAxR ;
+uint16_t rAxL ;
+//XM
+uint16_t rXmR ;
+uint16_t rXmL ;
+//IMU
+int16_t rXrate;
+int16_t rYrate;
+int16_t rZrate;
+int16_t rXacc;
+int16_t rYacc;
+int16_t rZacc;
+int16_t rXmag;
+int16_t rYmag;
+int16_t rZmag;
 };
 #endif
